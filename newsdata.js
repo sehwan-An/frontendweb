27a5015ca5f010ec46932c662b7003f5ce169935
sehwan <ash4104@naver.com>
2025-02-18 10:54:24 +0900

    깃헙 배포

--- newsdata.js
+++ newsdata.js
@@ -1,9 +1,7 @@
 //api key
-const newsdata_api = "pub_69488c042c584e95264197ca3cb9ee325c365";
-const query = "pizza";
+const api_key = "pub_69488c042c584e95264197ca3cb9ee325c365";
 const url = "https://newsdata.io/api/1/latest";
-const request = `${url}?apikey=${newsdata_api}&q=${query}`;
-console.log(request);
+// console.log(request);
 // window.addEventListener("DOMContentLoaded", ()=> {
 //     fetchNews();
 // });
@@ -12,3 +10,28 @@ console.log(request);
 //     const response = await fetch(request);
 //     console.log(response, typeof response);
 // }
+
+//1.DOM 탐색
+const newsForm = document.querySelector("#news_form");
+
+newsForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let query = document.querySelector("#keyword");
+  if (query == "") {
+    alert("검색어를 입력해야 합니다");
+    query.focus();
+    return;
+  }
+  // let search = document.querySelector("#keyword").value;
+
+  fetchNews(query.value);
+});
+
+function fetchNews(keyword) {
+  const req_url = `${url}?apikey=${api_key}&q=${query}`;
+  const response = fetchNews(req_url);
+  {
+    console.log(response);
+  }
+  console.log(keyword, "를 입력했습니다.");
+}
